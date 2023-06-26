b3063bec766d5cfd2d9ac5bf52f212acaaa4a3f2
Jorge Cordova <55065873+jcordovaj@users.noreply.github.com>
2023-06-26 03:14:47 -0400

    Update main.py cambios en votos

--- main.py
+++ main.py
@@ -112,18 +112,18 @@ def votos_titulo(titulo: str):
     ergo, no se devuelve ningun valor.
     """
     # Convertir el título a minúsculas
-    v_titulo = titulo.title()
-    query = df_work['title'] == v_titulo
+    #v_titulo = titulo.title()
+    query = df_work['title'] == titulo
     movie = df_work.loc[query, ['title', 'release_year', 'vote_count', 'vote_average']].head(1)
     if not movie.empty:
         v_titulo       = movie['title'].iloc[0]
-        v_med_votos    = movie['vote_average'].iloc[0]
+        v_prom_votos    = movie['vote_average'].iloc[0]
         v_qtty_votos   = movie['vote_count'].iloc[0]
         v_anio_estreno = movie['release_year'].iloc[0]
         if v_qtty_votos < 2000:
             return {"Puntaje insuficiente!!"}
         else:
-            return {"La película": v_titulo, "fue estrenada en el año": v_anio_estreno, " sus votos totales son": v_qtty_votos,
-                    "con un promedio de": v_med_votos}
+            return {"La película": titulo, "fue estrenada en el año": v_anio_estreno, " sus votos totales son": v_qtty_votos,
+                    "con un promedio de": v_prom_votos}
     else:
-        return {"No se encontró la película": v_titulo}
+        return {"No se encontró la película": titulo}
